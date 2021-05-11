4d5491faf70e2eba22d103c689df966829af12a8
gideonekekeke <71523437+gideonekekeke@users.noreply.github.com>
2021-05-11 12:57:49 +0100

    the git ignore

---
+++ index.js
@@ -0,0 +1,35 @@
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const fileRoutes = require("./routes/file-upload-routes");
+
+const url =
+  "mongodb+srv://shotkode:shotkode@cluster0.2kfdg.mongodb.net/shotkodeDB?retryWrites=true&w=majority";
+
+const port = process.env.PORT || 4000;
+// const port = 58990;
+
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("successfully connected");
+  });
+
+// const port = process.env.PORT || 8020;
+const app = express();
+app.use(cors());
+
+// require("./database")();
+
+app.use(bodyParser.json());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.use("/api", fileRoutes.routes);
+
+app.listen(process.env.PORT || 6070, () => console.log("loading"));
